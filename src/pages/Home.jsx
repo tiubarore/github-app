e18978dd5dcc9a0d7b677c895eb7b2221df8edbe
tiubarore <tiubarore93@gmail.com>
2024-11-13 11:58:06 +0100

    edited typo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import UserResults from "../compontents/users/UserResults";
-import UserSearch from "../compontents/users/USerSearch";
+import UserSearch from "../compontents/users/UserSearch";
 
 function Home() {
   return (
